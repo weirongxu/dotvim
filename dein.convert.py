f13b5b442abc0361a4c6c202f12768c9cc1e956c
Weirong Xu <weirongxu.raidou@gmail.com>
2016-09-04 20:16:29 +0800

    update

--- dein.convert.py
+++ dein.convert.py
@@ -3,6 +3,8 @@ import os
 import yaml
 import json
 import sys
+import re
+import traceback
 
 class Config(object):
   def __init__(self, f):
@@ -11,13 +13,16 @@ class Config(object):
     self.add_plugins(self.configs.get('plugins', []))
 
   def load(self, f):
-    return yaml.load(f)
+    lines = [line for line in f.readlines() if re.search(r'^\s*#', line) is None]
+    return yaml.load(''.join(lines))
 
   def add_plugins(self, plugins):
     for plugin in plugins:
       self.add_plugin(plugin)
 
   def add_plugin(self, plugin):
+    def parse_eval(str):
+      return re.sub(r'EVAL\((.*)\)', '"."\\1"."', str)
     if 'repo' in plugin:
       repo = plugin['repo']
       options = {k: v for k, v in plugin.items() if k != 'repo'}
@@ -26,7 +31,7 @@ class Config(object):
           "call dein#add('{}')".format(repo))
       else:
         self.vimscript_content.append(
-          "call dein#add('{}', {})".format(repo, json.dumps(options)))
+          "call dein#add('{}', {})".format(repo, parse_eval(json.dumps(options))))
     elif 'repos_include' in plugin:
       plugins = self.load(open(
         os.path.join(
@@ -45,4 +50,4 @@ try:
   target_file = open(target, 'w+')
   target_file.write(text)
 except Exception as e:
-  print(e)
+  traceback.print_exc()

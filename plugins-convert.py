f09a0b722e2213bbc5f99b8684d2f49412420e15
Weirong Xu <weirongxu.raidou@gmail.com>
2017-05-26 15:30:49 +0800

    change plugins.yml compiler

--- plugins-convert.py
+++ plugins-convert.py
@@ -4,22 +4,23 @@ import yaml
 import json
 import sys
 import re
+import glob
 import traceback
 
 class Config(object):
-  def __init__(self, source_file, include_root_dir):
+  def __init__(self, source_file, cwd):
     self.configs = self.load(source_file) or []
-    self.include_root_dir = include_root_dir
+    self.cwd = cwd
 
   def load(self, source_file):
     lines = [line for line in source_file.readlines() if re.search(r'^\s*#', line) is None]
     return yaml.load(''.join(lines))
 
-  def add_plugins(self, plugins, target_type):
+  def add_plugins(self, plugins, manager_type):
     for plugin in plugins:
-      self.add_plugin(plugin, target_type)
+      self.add_plugin(plugin, manager_type)
 
-  def add_plugin(self, plugin, target_type):
+  def add_plugin(self, plugin, manager_type):
     def parse_eval(str):
       str = re.sub(r'"EVAL\((.*)\)"', '\\1', str)
       str = re.sub(r'EVAL\((.*)\)', '".\\1."', str)
@@ -27,36 +28,35 @@ class Config(object):
       str = re.sub(r'\."",', ',', str)
       return str
 
-    def append_line(repo, options={}):
-      if target_type == 'vim-plug':
+    if 'repo' in plugin:
+      repo = plugin['repo']
+      options = {k: v for k, v in plugin.items() if k != 'repo'}
+
+      if manager_type == 'vim-plug':
         template = "Plug '{}', {}"
-      elif target_type == 'dein.vim':
+      elif manager_type == 'dein.vim':
         template = "call dein#add('{}', {})"
       self.vimscript_content.append(
         template.format(repo, parse_eval(json.dumps(options))))
 
-    if 'repo' in plugin:
-      repo = plugin['repo']
-      options = {k: v for k, v in plugin.items() if k != 'repo'}
-      append_line(repo, options)
     elif 'repos_include' in plugin:
-      plugins = self.load(open(
-        os.path.join(self.include_root_dir, plugin['repos_include'])
-      ))
-      self.add_plugins(plugins or [], target_type)
+      for path in glob.glob(os.path.join(self.cwd, plugin['repos_include'])):
+        self.vimscript_content.append(Config(open(path), os.path.dirname(path)).get_vim_script(manager_type))
 
-  def get_vim_script(self, target_type):
+  def get_yaml_list():
+    pass
+
+  def get_vim_script(self, manager_type):
     self.vimscript_content = []
-    self.add_plugins(self.configs, target_type)
+    self.add_plugins(self.configs, manager_type)
     return '\n'.join(self.vimscript_content)
 
 source = sys.argv[1]
 target = sys.argv[2]
-target_type = sys.argv[3]
-include_root_dir = sys.argv[4]
+manager_type = sys.argv[3]
 
 try:
-  text = Config(open(source), include_root_dir).get_vim_script(target_type)
+  text = Config(open(source), os.path.dirname(source)).get_vim_script(manager_type)
   target_file = open(target, 'w+')
   target_file.write(text)
 except Exception as e:
--- plugins-convert.rc.vim
+++ plugins-convert.rc.vim
@@ -1,26 +1,22 @@
-let s:source = $MY_VIMFILES.'/plugins.rc.yml'
 let s:target = $MY_VIMFILES.'/plugins-compiled.rc.vim'
-let s:include_dir = $MY_VIMFILES.'/plugins-repos'
+let s:plugins_dir = $MY_VIMFILES.'/plugins'
+let s:source = s:plugins_dir.'/index.rc.yml'
 let s:target_type = 'dein.vim'
 
 function! PluginsParse() "{{{
-  let ret = system($MY_VIMFILES.'/plugins-convert.py ' . s:source . ' ' . s:target . ' ' . s:target_type . ' ' . s:include_dir, '2>&1')
+  let ret = system($MY_VIMFILES.'/plugins-convert.py ' . s:source . ' ' . s:target . ' ' . s:target_type, '2>&1')
   if !empty(ret)
     echo ret
   endif
 endfunction "}}}
 
 function! s:checkUpdate() "{{{
-  if getftime(s:source) > getftime(s:target)
-    call PluginsParse()
-  else
-    for name in split(glob(s:include_dir . '/*.yml'), '\n')
-      if getftime(name) > getftime(s:target)
-        call PluginsParse()
-        break
-      endif
-    endfor
-  endif
+  for name in split(glob(s:plugins_dir . '/**/*.yml'), '\n')
+    if getftime(name) > getftime(s:target)
+      call PluginsParse()
+      break
+    endif
+  endfor
 endfunction "}}}
 
 call s:checkUpdate()

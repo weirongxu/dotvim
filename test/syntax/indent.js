80ca269ef04d8e5e6f1aaf9c4dfdd32c22db0199
Raidou <weirongxu.raidou@gmail.com>
2017-07-05 12:12:52 +0800

    fix js indent

---
+++ test/syntax/indent.js
@@ -0,0 +1,81 @@
+async function func(
+  test,
+  test2
+) {
+  await js()
+  hello()
+}
+
+// jQuery
+//
+{
+  [
+    'test',
+    'test2',
+  ],
+}
+
+/**
+ *
+ *
+ */
+function test(
+  count: number,
+  list: Array
+) {
+}
+
+Promise.all().then(() => {
+}).then(() => {
+})
+
+Promise.all()
+  .then(() => {
+  })
+  .then(() => {
+  })
+
+var a = {
+  a() {
+  },
+  c() {
+    console.log('c')
+    function () {
+    }
+  },
+}
+
+function() {
+}
+
+let c = 1
+
+import test from 'test'
+
+@decorator
+class Test {
+  @decorator
+  static a = 1
+
+  @decorator
+  static hello(a: number) {
+  }
+
+  @decorator
+  a() {
+  }
+}
+
+for (let it of []) {
+}
+
+a.b()
+
+var a = 1
+var b = 1
+
+@test
+function test() {
+}
+
+;[].forEach()

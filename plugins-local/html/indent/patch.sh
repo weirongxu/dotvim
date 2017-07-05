769fe7b1c214a44e89e22693a91879d396421dba
Weirong Xu <weirongxu.raidou@gmail.com>
2017-07-05 20:07:07 +0800

    update web indent

---
+++ plugins-local/html/indent/patch.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+patch /usr/share/nvim/runtime/indent/html.vim html.patch -o html.vim
---
+++ test/syntax/indent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  import name from 'name'
+
+  class Test {
+    @readOnly
+    static func() {
+    }
+  }
+</script>

456adf3f29e18dd61236eee44b9b767126b44fa6
Weirong Xu <weirongxu.raidou@gmail.com>
2017-07-17 12:09:55 +0800

    update html indent

--- plugins-local/html/indent/update-source.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# curl -L https://raw.githubusercontent.com/vim/vim/master/runtime/indent/html.vim -o source-html.vim
-curl -L https://raw.githubusercontent.com/othree/html5.vim/master/indent/html.vim -o source-html.vim
